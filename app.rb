67181b065d479c94e3e63d6fe55a4ee551540006
Paul M. Lambert <plambert@plambert.net>
2022-03-15 18:50:42 -0700

    added Dockerfile, translated a comment, added very limited caching, disabled web-based editing

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require 'erubis'
 
 events=[[0,1,2,3,4,5,6,7],
 		[0,{"type"=>"tomb","name"=>"Barbarian Cairn"},2,{"type"=>"trap","name"=>"Trap-Filled Glade"},4,{"type"=>"structure","name"=>"Oleg's Trade Post"},{"type"=>"hut","name"=>"Bokken's Hut"},7],
-		[0,1,{"type"=>"tomb","name"=>"Dead Trapper"},{"type"=>"monster","name"=>"Fairy Nest"},{"type"=>"plant","name"=>"Raddish Patch"},{"type"=>"monster","name"=>"Spider's Nest"},6,7],
+		[0,1,{"type"=>"tomb","name"=>"Dead Trapper"},{"type"=>"monster","name"=>"Fairy Nest"},{"type"=>"plant","name"=>"Radish Patch"},{"type"=>"monster","name"=>"Spider's Nest"},6,7],
 		[0,1,{"type"=>"landmark","name"=>"Frog Pond"},{"type"=>"ruin","name"=>"Temple of Elk"},{"type"=>"bridge","name"=>"Thorn River Camp"},5,6,7],
 		[0,1,2,3,4,5,6,7],
 		[0,1,2,3,4,5,6,7],
@@ -13,7 +13,7 @@ events=[[0,1,2,3,4,5,6,7],
 		[0,1,2,3,4,5,6,7],
 		[0,1,2,3,4,5,6,7]]
 
-## zero per non vista, "no" per ancora da esplorare, "yes" per esplorata
+# zero for unseen, "no" for seen but unexplored, "yes" for fully explored
 discovery=[
 	[0,0,0,0,"yes","yes","yes","yes"],
 	[0,0,0,0,0,"yes",0,0],
@@ -27,16 +27,19 @@ discovery=[
 	[0,0,0,0,0,0,0,0],
 	[0,0,0,0,0,0,0,0]]
 
+set :static_cache_control, [:public, :must_revalidate, :max_age => 10]
+
 get '/' do
+	cache_control :public, :must_revalidate, :max_age => 60
   @page_title = "Test hex"
   @hex=events
   @exploration=discovery
   erubis :index
 end
 
-get '/hexstatus/:row/:tile/:status' do
-  discovery[params[:row].to_i][params[:tile].to_i]=(params[:status] == "reset" ? 0 : params[:status])
-  "updated row:"+params[:row]+" tile:"+params[:tile]+" with "+params[:status]
-end
+# get '/hexstatus/:row/:tile/:status' do
+#   discovery[params[:row].to_i][params[:tile].to_i]=(params[:status] == "reset" ? 0 : params[:status])
+#   "updated row:"+params[:row]+" tile:"+params[:tile]+" with "+params[:status]
+# end
 
 
