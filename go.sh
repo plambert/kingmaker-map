04f912dd411b259bbdfcb4b21da075f2852f75a3
Paul M. Lambert <plambert@plambert.net>
2024-06-14 18:52:32 -0700

    more exploration, and write logs to file in home directory

--- go.sh
+++ go.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 
-exec bundle exec rackup -p 4567 # --host 0.0.0.0
+set -e -o pipefail
+
+mkdir -p ~/logs/kingmaker
+
+exec bundle exec rackup -p 4567 # --host 0.0.0.0 >> ~/logs/kingmaker/kingmaker.log 2>&1
+
 
